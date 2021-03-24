82b5c0c13d5752af1ffab3365661f7387e346eb4
noort <noort@10.1.10.35>
2021-03-23 21:15:29 -0400

    broken code

--- src/ntk/group/three/TeamThreeDemo.java
+++ src/ntk/group/three/TeamThreeDemo.java
@@ -1,22 +1,24 @@
 package ntk.group.three;
 
 public class TeamThreeDemo {
-	
+
 	public static void main(String[] args) {
 
-System.out.println("My first github commit....");
-		
+		System.out.println("My first github commit....");
+
 		System.out.println("test test");
 		addDemo();
 	}
-	
+
 	// add a code and shared with the team
 	public static void addDemo() {
-		System.out.println(1+2);
+		System.out.println(1 + 2);
 	}
 	
 	
 	
+	public static void todo() {
+		System.out.println("Follow the process");
 	}
 
-
+}
