bff4a73de70298f6ea35f94eb25aeec8debb9ca4
ahmedal-mubarak <ahmedal-mubarak@ahmeds-mbp.fios-router.home>
2021-03-23 21:08:07 -0400

    Method addDemo added

--- src/ntk/group/three/TeamThreeDemo.java
+++ src/ntk/group/three/TeamThreeDemo.java
@@ -3,7 +3,20 @@ package ntk.group.three;
 public class TeamThreeDemo {
 	
 	public static void main(String[] args) {
-		System.out.println("Team One ");
+
+System.out.println("My first github commit....");
+		
+		System.out.println("test test");
+		addDemo();
+	}
+	
+	// add a code and shared with the team
+	public static void addDemo() {
+		System.out.println(1+2);
+	}
+	
+	
+	
 	}
 
-}
+
