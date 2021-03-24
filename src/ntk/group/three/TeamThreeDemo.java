65e4f13927af9b72d93018f0a124c1e4c7bcc123
helen <helen@192.168.0.4>
2021-03-23 20:27:18 -0500

    Added print on TeamThreeDemo

--- src/ntk/group/three/TeamThreeDemo.java
+++ src/ntk/group/three/TeamThreeDemo.java
@@ -1,24 +1,11 @@
 package ntk.group.three;
 
 public class TeamThreeDemo {
-
+	
 	public static void main(String[] args) {
-
-		System.out.println("My first github commit....");
-
-		System.out.println("test test");
-		addDemo();
-	}
-
-	// add a code and shared with the team
-	public static void addDemo() {
-		System.out.println(1 + 2);
-	}
-	
-	
-	
-	public static void todo() {
-		System.out.println("Follow the process");
+		System.out.println("Team One ");
+		
+		System.out.println("Let's Stay Away from Broken Code");
 	}
 
 }
